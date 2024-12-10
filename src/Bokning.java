456af9e1e22ab07b4f1a6ee843deffabced2f5f2
Pontus Häggblom <pbhaggblom@gmail.com>
2024-12-10 14:12:27 +0100

    Lägger till funktionalitet för att boka och avboka tider

--- src/Bokning.java
+++ src/Bokning.java
@@ -1,14 +1,43 @@
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Bokning {
+public class Bokning implements Serializable {
 
-    LocalDate datum;
-    String namn;
-    String mail;
+    private LocalDate datum;
+    private String namn;
+    private String mail;
+
+    public Bokning(LocalDate datum) {
+        this.datum = datum;
+    }
 
     public Bokning(LocalDate datum, String namn, String mail) {
         this.datum = datum;
         this.namn = namn;
+        this.mail = mail;
+    }
+
+    public LocalDate getDatum() {
+        return datum;
+    }
+
+    public void setDatum(LocalDate datum) {
+        this.datum = datum;
+    }
+
+    public String getNamn() {
+        return namn;
+    }
+
+    public void setNamn(String namn) {
+        this.namn = namn;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
         this.mail = mail;
     }
 }
--- src/Sida.java
+++ src/Sida.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public abstract class Sida {
@@ -13,5 +14,17 @@ public abstract class Sida {
         } else if (input.equals("n")) {
             System.exit(0);
         }
-    };
+    }
+
+    public int läsInput() {
+        Scanner scan = new Scanner(System.in);
+        int val = 0;
+        try {
+            val = scan.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Välj 1 eller 2");
+            val = läsInput();
+        }
+        return val;
+    }
 }
